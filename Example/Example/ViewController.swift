2d9659c5a4ac17f3490302c60294de51b38aedbe
zhuxiangpeng <zxpwork@gmail.com>
2016-09-13 19:22:36 +0800

    添加文件

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhuxiangpeng on 16/9/13.
+//  Copyright © 2016年 zxp. All rights reserved.
+//
+
+import UIKit
+import QuickStart
+
+class ViewModel: NSObject {
+    
+}
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ QuickStart/MVCCore.swift
@@ -0,0 +1,22 @@
+//
+//  MVCCore.swift
+//  QuickStart
+//
+//  Created by zhuxiangpeng on 16/9/13.
+//  Copyright © 2016年 zxp. All rights reserved.
+//
+
+import UIKit
+
+//public protocol MVCCoreViewModel: NSObjectProtocol {
+//    
+//}
+//
+//public protocol MVCCoreViewController: NSObjectProtocol  {
+//    var viewModel: MVCCoreViewModel { get set }
+//}
+
+public class MVCCoreModel: NSObject {
+    
+    
+}
