ab3739e58944db5fd0ad27a04b72cbd968b81cc3
爱爱老婆 <zxpwork@gmail.com>
2016-09-16 10:09:25 +0800

    添加测试代码

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by zhu on 16/9/16.
+//  Copyright © 2016年 xpz. All rights reserved.
+//
+
+import UIKit
+import QuickStart
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
