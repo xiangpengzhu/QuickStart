e5c7004bb56c479c73b31a2d0b39cb24e94ac69b
爱爱老婆 <zxpwork@gmail.com>
2016-09-16 20:43:43 +0800

    添加规范MVC框架

--- QuickStart/MVCCore/MVCCore.swift
+++ QuickStart/MVCCore/MVCCore.swift
@@ -8,15 +8,65 @@
 
 import UIKit
 
-//public protocol MVCCoreViewModel: NSObjectProtocol {
-//    
-//}
-//
-//public protocol MVCCoreViewController: NSObjectProtocol  {
-//    var viewModel: MVCCoreViewModel { get set }
-//}
+//MARK: - extension stored property keys 
+fileprivate var viewControllerViewModelKey      = "viewControllerViewModelKey"
+fileprivate var viewViewModelKey                = "viewViewModelKey"
 
-public class MVCCoreModel: NSObject {
+//MARK: - ViewController
+public protocol MVCCoreViewController: NSObjectProtocol {
+    var viewModel: MVCCoreViewModel? { get set }
+}
+
+extension UIViewController: MVCCoreViewController {
+    public var viewModel: MVCCoreViewModel? {
+        get {
+            return objc_getAssociatedObject(self, &viewControllerViewModelKey) as? MVCCoreViewModel
+        }
+        set {
+            objc_setAssociatedObject(self, &viewControllerViewModelKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            newValue?.viewController = self
+        }
+    }
+}
+
+//MARK: - View
+extension UIView {
+    public var viewModel: MVCCoreViewModel? {
+        get {
+            return objc_getAssociatedObject(self, &viewViewModelKey) as? MVCCoreViewModel
+        }
+        set {
+            objc_setAssociatedObject(self, &viewViewModelKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            newValue?.view = self
+        }
+    }
+}
+
+//MARK: - viewModel
+public class MVCCoreViewModel: NSObject {
+    public weak var viewController: UIViewController?
+    public weak var view: UIView?
+}
+
+public class MVCCoreDataSourceAndDelegate: NSObject {
+    public weak var viewController: UIViewController?
+    public var viewModel: MVCCoreViewModel?
     
+    init(viewController: UIViewController, viewModel: MVCCoreViewModel) {
+        self.viewController = viewController
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    private override init() {
+        super.init()
+    }
+}
+
+public class MVCCoreTableViewDataSourceAndDelegate: MVCCoreDataSourceAndDelegate {
+}
+
+public class MVCCoreCollectionViewDataSourceAndDelegate: MVCCoreDataSourceAndDelegate {
     
 }
+
